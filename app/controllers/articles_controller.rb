44e9e569e225c03dc3d95af133b3430e6615e8d6
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-10-27 14:33:21 -0400

    seed data created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+
+    def index
+        articles = Article.all
+        render json: articles, except: [:updated_at, :created_at]
+    end
+
+    def show
+        article = Article.find(params[:id])
+        render json: article, except: [:updated_at, :created_at]
+    end
+
+    def create
+        article = Article.create(article_params)
+
+    end
+    
+    def update
+        article = Article.find(params[:id])
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :author, :publication_date, :body)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'date'
+
+christa = User.create(name: "Christa Gammage")
+
+cows = Article.create(title: "Cows", author: "Lola Stewart", publication_date: DateTime.now, body: "iukjrg0iojerdglkasdfiouw4etasdf")
+cats = Article.create(title: "Cats", author: "George Wash", publication_date: DateTime.now, body: "iukjrg0iojerdglkasdfiuyjghb;kj,nouw4etasdf")
+birds = Article.create(title: "Birds", author: "John Smith", publication_date: DateTime.now, body: "iukjrg0iojerdglkasdfiojkkhhkuw4etasdf")
